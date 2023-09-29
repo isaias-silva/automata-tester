f40e3eed155cf14b525c31cf3f6310cab8c8470a
isaias-silva <zackblackmangold@gmail.com>
2023-09-29 12:03:55 -0300

    front

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default axios.create(
     {
-        baseURL:`http://167.86.126.146:8080/`,
+        baseURL:`http://localhost:8080`,
         headers: {
             'Content-Type': 'application/json',
 
